16b4331b8cf6997645c9e2a138c0ba236f4777c3
mohammedmosaad <mohammedmosaad869@gmail.com>
2024-07-05 01:36:16 +0300

    create notes view model

---
+++ lib/model/note_model.dart
@@ -0,0 +1,12 @@
+class NoteModel {
+  final String title, desc;
+  final DateTime dateTime;
+  final int color;
+
+  NoteModel({
+    required this.title,
+    required this.desc,
+    required this.dateTime,
+    required this.color,
+  });
+}
