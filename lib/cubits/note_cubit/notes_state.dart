40a5c50d20225e5661367c6da4bfdfeb9e9c397a
mohammedmosaad <mohammedmosaad869@gmail.com>
2024-07-06 14:17:39 +0300

    display data part1

--- lib/cubits/note_cubit/notes_state.dart
+++ lib/cubits/note_cubit/notes_state.dart
@@ -1,18 +1,3 @@
-import 'package:notes/model/note_model.dart';
-
 class NotesState {}
 
 final class NotesInitial extends NotesState {}
-
-final class NotesLoadingState extends NotesState {}
-
-final class NotesSuccessfulState extends NotesState {
-  final List<NoteModel> notes;
-
-  NotesSuccessfulState(this.notes);
-}
-
-final class NotesFailureState extends NotesState {
-  final String errorMessage;
-  NotesFailureState(this.errorMessage);
-}
--- lib/widgets/notes_listView.dart
+++ lib/widgets/notes_listView.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:notes/cubits/note_cubit/notes_cubit.dart';
+import 'package:notes/model/note_model.dart';
 
+import '../cubits/note_cubit/notes_state.dart';
 import 'notes_item.dart';
 
 class NotesListView extends StatelessWidget {
@@ -7,18 +11,24 @@ class NotesListView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(vertical: 16),
-      child: ListView.separated(
-        padding: EdgeInsets.zero,
-        separatorBuilder: (context, index) => const SizedBox(height: 10),
-        shrinkWrap: true,
-        physics: const BouncingScrollPhysics(),
-        itemBuilder: (ctx, index) {
-          return const NotesItem();
-        },
-        itemCount: 10,
-      ),
+    return BlocBuilder<NotesCubit, NotesState>(
+      builder: (context, state) {
+        List<NoteModel> notesList =
+            BlocProvider.of<NotesCubit>(context).notes ?? [];
+        return Padding(
+          padding: const EdgeInsets.symmetric(vertical: 16),
+          child: ListView.separated(
+            padding: EdgeInsets.zero,
+            separatorBuilder: (context, index) => const SizedBox(height: 10),
+            shrinkWrap: true,
+            physics: const BouncingScrollPhysics(),
+            itemBuilder: (ctx, index) {
+              return const NotesItem();
+            },
+            itemCount: notesList.length,
+          ),
+        );
+      },
     );
   }
 }
